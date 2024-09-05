8b90e1c052f0a39ffc63453a8da28b885810af72
yashbhagwat2005 <yash3works@gmail.com>
2024-09-05 14:51:53 +0530

    Update and rename 16. PROGRAM TO READ THREE NOS. AND PRINT MAX. to #16Read_3_number_mac.c

--- #16Read_3_number_mac.c
+++ #16Read_3_number_mac.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 int main(){
     int a,b,c;
     printf("Enter the first number: ");
@@ -5,7 +6,7 @@ int main(){
     printf("Enter the second number: ");
     scanf("%d",&b);
     printf("Enter the third number: ");
-    scanf("%d",&b);
+    scanf("%d",&c);
     if (a>b){
         if (a>c){
             printf("%d is the greatest number between the three given numbers",a);
@@ -14,7 +15,7 @@ int main(){
         }else{
             printf("%d and %d are both equal",a,c);
         }
-    else if (b>a){
+    }else if (b>a){
         if (b>c){
             print("%d is the greatest number between the three given numbers",b);
         
@@ -24,8 +25,8 @@ int main(){
         else{
             printf("%d and %d are both equal",b,c);
         }
-    else if (a==b && b==c){
-        printf("All the given number are equal");
+    }else if (a==b && b==c){
+        printf("All the given numbers are equal");
     }
         
     }
