569725ee8b14ef2a07f35015633b989103682ab6
yashbhagwat2005 <yash3works@gmail.com>
2024-09-18 21:02:41 +0530

    Update and rename #20Simple_calcullator_using_switch_case.c to IOP LAB/LAB_2/#20Simple_calcullator_using_switch_case.c

--- IOP LAB/LAB_2/#20Simple_calcullator_using_switch_case.c
+++ IOP LAB/LAB_2/#20Simple_calcullator_using_switch_case.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
 int main() {
+    // declaring u_operator(input from the user where the user specifies the operator) as a integer
     int u_operator;
+    // declaring num_1,num_2(2 numbers from the user to use the operator between them) as a float
     float num_1, num_2;
-
+    // displaying the menu for the user 
     printf("Welcome to the calculator. Please choose an operation:\n");
     printf("1. Addition (+)\n");
     printf("2. Subtraction (-)\n");
     printf("3. Division (/)\n");
-
+    // user input for u_operator
     printf("Please enter the number for the operation: ");
     scanf("%d", &u_operator);
-
+    //input for num_1
     printf("Enter the first number: ");
     scanf("%f", &num_1);
+    // input for num_2
     printf("Enter the second number: ");
     scanf("%f", &num_2);
-
     switch (u_operator) {
         case 1:
             printf("The output is %f\n", num_1 + num_2);
@@ -31,6 +33,7 @@ int main() {
                 printf("Error: Division by zero\n");
             }
             break;
+        // if the user enters a number other than the given options in the menu then it will print error
         default:
             printf("Error: Invalid choice\n");
             return 1;
