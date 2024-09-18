534eaa8cc2556e4d7c6729ecaa52e06ec72726d5
yashbhagwat2005 <yash3works@gmail.com>
2024-09-18 20:07:33 +0530

    Update and rename #15grade_the students_marks.c to IOP LAB/LAB_1/#15grade_the students_percentage.c

--- IOP LAB/LAB_1/#15grade_the students_percentage.c
+++ IOP LAB/LAB_1/#15grade_the students_percentage.c
@@ -15,19 +15,21 @@ int main(){
     //calculating the percentage 
     percentage = ((s_1+s_2+s_3+s_4+s_5)/500)*100;
     //if else logic for what grade was secured 
-    if (90<percentage){
-        printf("You have secured grade A \n");
-    }else if (80<percentage && percentage<=90){
-        printf("You have secured grade B \n");
-    }else if (70<percentage && percentage<=80){
-        printf("You have secured grade C \n");
-    }else if (60<percentage && percentage<=70){
-        printf("You have secured grade D \n");
-    }else if (percentage<=60){
-        printf("You have secured grade F \n");
-    }else if (percentage<0||percentage>100){
-        printf("Please enter the marks correctly");
+   if (percentage < 0 || percentage > 100) {
+        printf("Please enter a valid percentage between 0 and 100.\n");
         return 1;
+    }
+    // Grading logic
+    if (percentage > 90) {
+        printf("You have secured grade A \n");
+    } else if (percentage > 80) {
+        printf("You have secured grade B \n");
+    } else if (percentage > 70) {
+        printf("You have secured grade C \n");
+    } else if (percentage > 60) {
+        printf("You have secured grade D \n");
+    } else {
+        printf("You have secured grade F \n");
     }
     printf("You have scored %f percentage",percentage);
    
