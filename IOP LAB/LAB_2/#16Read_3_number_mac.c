7c0e4a42a50fa7d6adc95035788cae2d7c07a648
yashbhagwat2005 <yash3works@gmail.com>
2024-09-18 20:14:28 +0530

    Update and rename #16Read_3_number_mac.c to IOP LAB/LAB_2/#16Read_3_number_mac.c

--- IOP LAB/LAB_2/#16Read_3_number_mac.c
+++ IOP LAB/LAB_2/#16Read_3_number_mac.c
@@ -1,26 +1,35 @@
 #include<stdio.h>
 int main(){
+    // declaring a(1st_input),b(2nd_input),c(3rd_input)
     int a,b,c;
+    // input for a
     printf("Enter the first number: ");
     scanf("%d",&a);
+    // input for b
     printf("Enter the second number: ");
     scanf("%d",&b);
+    // input for c
     printf("Enter the third number: ");
     scanf("%d",&c);
+    // if a is greater than b
     if (a>b){
+        // if a is greater than c then it is the greatest between the three numbers
         if (a>c){
             printf("%d is the greatest number between the three given numbers",a);
+        // if c is greater than a then it is the greates among the three numbers 
         }else if (c>a){
             printf("%d is the greatest number between the three given numbers",a);
         }else{
+        // a and b are equal
             printf("%d and %d are both equal",a,c);
         }
+    // same logic for b
     }else if (b>a){
         if (b>c){
-            print("%d is the greatest number between the three given numbers",b);
+            printf("%d is the greatest number between the three given numbers",b);
         
         }else if(c>b){
-            printf("%d is the greatest number between the three given numbers",c)
+            printf("%d is the greatest number between the three given numbers",c);
         }
         else{
             printf("%d and %d are both equal",b,c);
@@ -29,7 +38,8 @@ int main(){
         printf("All the given numbers are equal");
     }
         
-    }
+    
 }
+
 
 // =)
