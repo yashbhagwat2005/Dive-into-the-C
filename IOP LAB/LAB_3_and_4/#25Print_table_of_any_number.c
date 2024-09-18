4ac6b3dc4adf24baf27f4e7d896646a4183df63b
yashbhagwat2005 <yash3works@gmail.com>
2024-09-18 21:36:32 +0530

    Update and rename #25Print_table_of_any_number.c to IOP LAB/LAB_3_and_4/#25Print_table_of_any_number.c

--- IOP LAB/LAB_3_and_4/#25Print_table_of_any_number.c
+++ IOP LAB/LAB_3_and_4/#25Print_table_of_any_number.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 
+#include <stdio.h>
 int main() {
     // declaring number as a integer the user gives the input for this variable
     int number;
     // declaring i as a integer 
     int i;
-    printf("Enter a number to print the multiplication table of the given number: of the given number
+    printf("Enter a number to print the multiplication table of the given number: ");
     scanf("%d",&number);
     i=1;
     // using while statement to print table of the given number 
