a0594d52b2d789b05fba7ad05b9e146bf5d73a1e
yashbhagwat2005 <yash3works@gmail.com>
2024-09-18 20:19:23 +0530

    Update and rename #17Print_max_using_logical_operators.c to IOP LAB/LAB_2/#17Print_max_using_logical_operators.c

--- IOP LAB/LAB_2/#17Print_max_using_logical_operators.c
+++ IOP LAB/LAB_2/#17Print_max_using_logical_operators.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    // declaring a,b,c as an integer 
+    // declaring a(first input from the user),b(second input from the user),c(third input from the user) as an integer 
     int a, b, c;
-    // a is the first number
+    // input for "a" from the user
     printf("Enter the first number: ");
     scanf("%d", &a);
-    // b is the second number
+    // input for "b" from the user
     printf("Enter the second number: ");
     scanf("%d", &b);
-    // c is the second number
+    // input for "c" from the user
     printf("Enter the third number: ");
     scanf("%d", &c);
-    // logic to find the greatest between the three numbers
+    // logic to find the greatest between the three numbers using && operators
     if (a > b && a > c) {
         printf("%d is the greatest between the three\n", a);
     } else if (b > c && b > a) {
@@ -26,3 +26,5 @@ int main() {
     return 0;
 }
 
+
+// =)
