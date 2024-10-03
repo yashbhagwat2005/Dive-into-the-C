cbe85744e57ee9d68df6dbc648447c83ad7d9e5c
yashbhagwat2005 <yash3works@gmail.com>
2024-10-03 14:51:39 +0530

    Create #67diffrence_of_2_matrix.c

---
+++ IOP LAB/LAB_5_and_6/#67diffrence_of_2_matrix.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+// declaring array a1[3][3](2d array),a2[3][3](2d array),diff[3][3](diff of the 2 arrays) as a integer
+    int a1[3][3], a2[3][3], diff[3][3];
+    // input from the user for every element in matrix a1
+    printf("Enter elements of the first 3x3 matrix:\n");
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            scanf("%d", &a1[i][j]);
+        }
+    }
+    // input from the user for every element in matrix a2
+    printf("Enter elements of the second 3x3 matrix:\n");
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++)
+            scanf("%d", &a2[i][j]);
+    }
+    // loop for adding the corresponding element of matrix a and matrix b
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+          diff[i][j] = a1[i][j] - a2[i][j];}
+    }
+    printf("Resultant Matrix:\n");
+    // printing the resultant matrix
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++){
+            printf("%d ", diff[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
