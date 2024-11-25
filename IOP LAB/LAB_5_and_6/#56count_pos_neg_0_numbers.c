6017bc955c87f086c07cda1331aaa0d5561cddcb
yashbhagwat2005 <yash3works@gmail.com>
2024-11-25 16:08:29 +0530

    Update #56count_pos_neg_0_numbers.c

--- IOP LAB/LAB_5_and_6/#56count_pos_neg_0_numbers.c
+++ IOP LAB/LAB_5_and_6/#56count_pos_neg_0_numbers.c
@@ -2,7 +2,7 @@
 int main(){
     // declaring n(element input from the user),count_pos(total number of postive numbers),count_negative(total number of negative numbers)) as a int
     // decalring count_0 (counts the total number of zeroes) as a int
-    int n,count_pos,count_neg,count_0;
+    int n,count_pos = 0,count_neg = 0,count_0 = 0;
     // declaring a(10 elements in the array "a") as a int
     int a[10];
     // input from the user for n and adding it to the array a
@@ -23,7 +23,7 @@ int main(){
     }
     printf("\nTotal number of postive number: %d",count_pos);
     printf("\nTotal number of negative number: %d",count_neg);
-    printf("\nTotal number of negative number: %d",count_0);
+    printf("\nTotal number of 0 value elements : %d",count_0);
 
 }
 
