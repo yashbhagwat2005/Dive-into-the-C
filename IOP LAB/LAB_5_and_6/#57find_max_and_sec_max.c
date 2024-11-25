983e9a14b07a7ccaac42c93ece5e104467f15b2d
yashbhagwat2005 <yash3works@gmail.com>
2024-11-25 16:17:06 +0530

    Update #57find_max_and_sec_max.c

--- IOP LAB/LAB_5_and_6/#57find_max_and_sec_max.c
+++ IOP LAB/LAB_5_and_6/#57find_max_and_sec_max.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
+//prototyping a function with parameters of array,max,sec_max,result called max_sec_max to find the max number and sec max number in the the array "array"
+int max_sec_max(int array[],int max,int sec_max,int result[]);
 int main(){
-    // declaring n(input from the user for n  for the elements in the array),MAX(largest number in the array),SEC_MAC(second largest number in the array)
-    int n,MAX,SEC_MAX;
-    int a[10];
-    // input for n from the user and adding that number to array "a"
-    for (int i = 0; i<10 ; i++){
-        printf("\nEnter a number: ");
-        scanf("%d",&n);
-        a[i]=n;
-    // initially setting max equal to the first number in array
-    MAX = a[0];
-    // initially setting second max equal to the second number in array
-    SEC_MAX = a[0];
+    // declaring array of the size of elements of 10
+    int array[10];
+    //declaring result of the size of the elements of 2
+    int result[2];
+    // input from the user for the elements
+    for (int i = 0;i<10;i++){
+        printf("Enter a number: \n");
+        scanf("%d",&array[i]);
     }
-    // comparing every element of the array to the max and second max number 
-    for (int i = 1; i<10 ;i++){
-        if(a[i]>MAX){
-            SEC_MAX = MAX;
-            MAX = a[i];
-        }
-        else if (a[i]<MAX && a[i]>SEC_MAX){
-            SEC_MAX = a[i];
-        }
-    }
-    printf("\n The largest number in the array is %d",MAX);
-    printf("\n The second largest number in the array is %d",SEC_MAX);
+    // decalring max (initially max is equal to the first element of the array) sec_max(initially sec_max is equal to the second element of the array)
+    int max = array[0];
+    int sec_max = array[0];
+    // calling the function 
+    max_sec_max(array,max,sec_max,result);
+    printf("The firt element is  %d and the second element is %d",result[0],result[1]);
 }
-
-
-// =)
+int max_sec_max(int array[],int max,int sec_max,int result[]){
+    // any element that is bigger than max then the that elment is assigned to max and the other is assigned to sec_max
+    for (int i = 0;i<10;i++){
+        if (array[i]>max){
+            sec_max = max;
+            max = array[i];
+        }
+    }
+    // to return 2 values both max and sec_max are returned as an result
+    result[0] = max;
+    result[1] = sec_max;
+}
