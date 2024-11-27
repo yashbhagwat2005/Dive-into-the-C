90f064cf9a451cd7196380d66356dcef06edfa25
yashbhagwat2005 <yash3works@gmail.com>
2024-11-28 02:45:38 +0530

    Update #105.c

--- IOP LAB/LAB_9/#105.c
+++ IOP LAB/LAB_9/#105.c
@@ -1,26 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-// Function prototype for generating permutations
+// Function prototype to generate permutations
 void permute(char *str, int start, int end);
-
-int main() {
-    // Variable to store the string
-    char str[100];
-
-    // Input for the string
-    printf("Enter a string: ");
-    scanf("%s", str);
-
-    // Find the length of the string
-    int length = strlen(str);
-
-    // Call the permutation function
-    printf("All permutations of the string are:\n");
-    permute(str, 0, length - 1);
-
-    return 0;
-}
 
 // Function to swap two characters using pointers
 void swap(char *x, char *y) {
@@ -43,8 +25,35 @@ void permute(char *str, int start, int end) {
             // Recursively generate permutations for the rest of the string
             permute(str, start + 1, end);
 
-            // Backtrack: undo the swap
+            // Backtrack: undo the swap to restore the original string
             swap((str + start), (str + i));
         }
     }
+}
+
+int main() {
+    // Variable to store the string
+    char str[100];
+
+    // Input for the string
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    // Find the length of the string
+    int length = strlen(str);
+
+    // Sort the string to ensure permutations are in lexicographical order
+    for (int i = 0; i < length - 1; i++) {
+        for (int j = i + 1; j < length; j++) {
+            if (str[i] > str[j]) {
+                swap(&str[i], &str[j]);
+            }
+        }
+    }
+
+    // Call the permutation function
+    printf("The permutations of the string are:\n");
+    permute(str, 0, length - 1);
+
+    return 0;
 }
